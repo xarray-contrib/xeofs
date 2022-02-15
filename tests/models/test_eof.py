d9ca6ae97c3421bc352fe89cbd22b44bc6e9d548
nicrie <niclasrieger@gmail.com>
2022-02-15 23:20:39 +0100

    test: add more test cases for numpy wrapper

--- tests/models/test_eof.py
+++ tests/models/test_eof.py
@@ -3,8 +3,8 @@ import pytest
 import warnings
 from numpy.testing import assert_allclose
 
+from xeofs.models._eof_base import _EOF_base
 from xeofs.models import EOF
-
 
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
@@ -15,6 +15,7 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
     ('EOF', True, None)
 ])
 def test_solution(method, norm, weights, reference_solution, sample_array):
+    # Compare pandas implementation against reference solution
     experiment = reference_solution.get_experiment(
         method=method, norm=norm, weights=weights
     )
@@ -28,3 +29,32 @@ def test_solution(method, norm, weights, reference_solution, sample_array):
     assert_allclose(model.explained_variance_ratio(), reference['explained_variance_ratio'])
     assert_allclose(model.eofs(), reference['eofs'])
     assert_allclose(model.pcs(), reference['pcs'])
+
+
+@pytest.mark.parametrize('n_modes', (1, 5, None))
+def test_n_modes(n_modes, sample_array):
+    # Number of modes is defined by minimum of sample and feature number
+    data_no_nan = sample_array[:, ~np.isnan(sample_array).all(axis=0)]
+    base = _EOF_base(data_no_nan, n_modes=n_modes)
+
+    ref_n_modes = min(data_no_nan.shape) if n_modes is None else n_modes
+
+    assert base.n_modes == ref_n_modes
+
+
+def test_abstract_class(sample_array):
+    # Abstract class methods are not implemented
+    data_no_nan = sample_array[:, ~np.isnan(sample_array).all(axis=0)]
+    base = _EOF_base(data_no_nan)
+    with pytest.raises(Exception):
+        base.solve()
+    with pytest.raises(Exception):
+        base.singular_values()
+    with pytest.raises(Exception):
+        base.explained_variance()
+    with pytest.raises(Exception):
+        base.explained_variance_ratio()
+    with pytest.raises(Exception):
+        base.eofs()
+    with pytest.raises(Exception):
+        base.pcs()
