cf330ffb0dca2184398a1bb6bd49a3cc456af586
nicrie <niclasrieger@gmail.com>
2022-03-12 21:16:44 +0100

    refactor: tests for new transformer definition

--- tests/models/test_transformer.py
+++ tests/models/test_transformer.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 import warnings
 from numpy.testing import assert_array_equal
-from xeofs.models._array_transformer import _ArrayTransformer
+from xeofs.models._transformer import _ArrayTransformer, _MultiArrayTransformer
 
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
@@ -78,13 +78,13 @@ def test_invalid_transform_with_nan():
     None,
 ])
 def test_transform_weight_data_types(weights):
-    # Input data type equals output data type
+    # Output data type is always np.ndarray
     rng = np.random.default_rng(7)
     X = rng.standard_normal((5, 3))
     tf = _ArrayTransformer()
     _ = tf.fit(X)
     transformed = tf.transform_weights(weights)
-    assert type(weights) is type(transformed)
+    assert isinstance(transformed, np.ndarray)
 
 
 @pytest.mark.parametrize('weights', [
@@ -153,3 +153,43 @@ def test_invalid_back_transformation(shape, axis):
         _ = tf.back_transform_eofs(transformed[:, np.newaxis])
     with pytest.raises(Exception):
         _ = tf.back_transform_pcs(transformed[:, np.newaxis])
+
+
+def test_invalid_multi_transform():
+    # Differet number of samples raises error
+    shape1 = (10, 5, 4)
+    shape2 = (10, 8, 6)
+    rng = np.random.default_rng(7)
+    X1 = rng.standard_normal(shape1)
+    X2 = rng.standard_normal(shape2)
+    tf = _MultiArrayTransformer()
+    tf.fit([X1, X2])
+
+    new_shape1 = (3, 5, 4)
+    new_shape2 = (4, 8, 6)
+    rng = np.random.default_rng(7)
+    new_X1 = rng.standard_normal(new_shape1)
+    new_X2 = rng.standard_normal(new_shape2)
+    with pytest.raises(Exception):
+        tf.fit([new_X1, new_X2])
+    with pytest.raises(Exception):
+        tf.transform([new_X1, new_X2])
+
+
+def test_invalid_multi_transform_weights():
+    # Incorrect number of features raises error
+    shape1 = (10, 5, 4)
+    shape2 = (10, 8, 6)
+    rng = np.random.default_rng(7)
+    X1 = rng.standard_normal(shape1)
+    X2 = rng.standard_normal(shape2)
+    tf = _MultiArrayTransformer()
+    tf.fit([X1, X2])
+
+    weights_incorrect_shape1 = (5, 3)
+    weights_shape2 = (8, 6)
+    rng = np.random.default_rng(7)
+    weights1 = rng.standard_normal(weights_incorrect_shape1)
+    weights2 = rng.standard_normal(weights_shape2)
+    with pytest.raises(Exception):
+        tf.transform_weights([weights1, weights2])
