19bb84e3c57c4762fb2d61b3a60df143e6c05b72
nicrie <niclasrieger@gmail.com>
2022-08-22 14:58:35 +0200

    docs: fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,14 +15,11 @@
 import os
 import sys
 
-# from xeofs import __version__
+from xeofs import __version__
 
 sys.path.insert(0, os.path.abspath('..'))
 
-from importlib.metadata import version
-__version__ = version('xeofs')
 
-#
 # # prevent circular imports...
 # import sphinx.builders.html
 # import sphinx.builders.latex
@@ -82,7 +79,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
