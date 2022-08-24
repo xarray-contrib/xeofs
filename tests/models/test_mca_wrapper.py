900d76ec1298b3c604c8734a41d8335a9446249e
nicrie <niclasrieger@gmail.com>
2022-08-24 11:37:25 +0200

    test: add simple test cases for MCA Rotator

--- tests/models/test_mca_wrapper.py
+++ tests/models/test_mca_wrapper.py
@@ -8,55 +8,63 @@ from xeofs.pandas import MCA as pdMCA
 from xeofs.xarray import MCA as xrMCA
 
 
-def test_wrapper_bootstrapper(sample_array):
+@pytest.mark.parametrize('scaling, norm', [
+    (0, True),
+    (1, True),
+    (2, True),
+    (0, False),
+    (1, False),
+    (2, False),
+])
+def test_wrapper_bootstrapper(scaling, norm, sample_array):
     # Bootstrapping runs without errors
     X = sample_array
     df = pd.DataFrame(X)
     da = xr.DataArray(X)
     # Perform analysis with all three wrappers
-    numpy_model = MCA(X, X)
+    numpy_model = MCA(X, X, norm=norm)
     numpy_model.solve()
 
-    pandas_model = pdMCA(df, df)
+    pandas_model = pdMCA(df, df, norm=norm)
     pandas_model.solve()
 
-    xarray_model = xrMCA(da, da, dim='dim_0')
+    xarray_model = xrMCA(da, da, norm=norm, dim='dim_0')
     xarray_model.solve()
 
     # Numpy
     desired_svals = numpy_model.singular_values()
     desired_expcovar = numpy_model.explained_covariance()
     desired_scf = numpy_model.squared_covariance_fraction()
-    desired_svecs = numpy_model.singular_vectors()
-    desired_pcs = numpy_model.pcs()
+    desired_svecs = numpy_model.singular_vectors(scaling=scaling)
+    desired_pcs = numpy_model.pcs(scaling=scaling)
     desired_hom_pats, desired_pvals_hom = numpy_model.homogeneous_patterns()
     desired_het_pats, desired_pvals_het = numpy_model.heterogeneous_patterns()
-    desired_xproj = numpy_model.project_onto_left_singular_vectors(X)
-    desired_yproj = numpy_model.project_onto_right_singular_vectors(X)
+    desired_xproj = numpy_model.project_onto_left_singular_vectors(X, scaling=scaling)
+    desired_yproj = numpy_model.project_onto_right_singular_vectors(X, scaling=scaling)
     desired_Xrec, desired_Yrec = numpy_model.reconstruct_XY()
 
     # pandas
     actual_pd_svals = pandas_model.singular_values()
     actual_pd_expcovar = pandas_model.explained_covariance()
     actual_pd_scf = pandas_model.squared_covariance_fraction()
-    actual_pd_svecs = pandas_model.singular_vectors()
-    actual_pd_pcs = pandas_model.pcs()
+    actual_pd_svecs = pandas_model.singular_vectors(scaling=scaling)
+    actual_pd_pcs = pandas_model.pcs(scaling=scaling)
     actual_pd_hom_pats, actual_pd_pvals_hom = pandas_model.homogeneous_patterns()
     actual_pd_het_pats, actual_pd_pvals_het = pandas_model.heterogeneous_patterns()
-    actual_pd_xproj = pandas_model.project_onto_left_singular_vectors(df)
-    actual_pd_yproj = pandas_model.project_onto_right_singular_vectors(df)
+    actual_pd_xproj = pandas_model.project_onto_left_singular_vectors(df, scaling=scaling)
+    actual_pd_yproj = pandas_model.project_onto_right_singular_vectors(df, scaling=scaling)
     actual_pd_Xrec, actual_pd_Yrec = pandas_model.reconstruct_XY()
 
     # xarray
     actual_xr_svals = xarray_model.singular_values()
     actual_xr_expcovar = xarray_model.explained_covariance()
     actual_xr_scf = xarray_model.squared_covariance_fraction()
-    actual_xr_svecs = xarray_model.singular_vectors()
-    actual_xr_pcs = xarray_model.pcs()
+    actual_xr_svecs = xarray_model.singular_vectors(scaling=scaling)
+    actual_xr_pcs = xarray_model.pcs(scaling=scaling)
     actual_xr_hom_pats, actual_xr_pvals_hom = xarray_model.homogeneous_patterns()
     actual_xr_het_pats, actual_xr_pvals_het = xarray_model.heterogeneous_patterns()
-    actual_xr_xproj = xarray_model.project_onto_left_singular_vectors(da)
-    actual_xr_yproj = xarray_model.project_onto_right_singular_vectors(da)
+    actual_xr_xproj = xarray_model.project_onto_left_singular_vectors(da, scaling=scaling)
+    actual_xr_yproj = xarray_model.project_onto_right_singular_vectors(da, scaling=scaling)
     actual_xr_Xrec, actual_xr_Yrec = xarray_model.reconstruct_XY()
 
     # assert consistent pandas results
