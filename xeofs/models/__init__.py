79e82ccb0802079123d3254c0ef9a617a962fb90
Niclas Rieger <niclasrieger@gmail.com>
2023-09-26 03:32:13 +0200

    feat: add CCA support

--- xeofs/models/__init__.py
+++ xeofs/models/__init__.py
@@ -4,3 +4,19 @@ from .opa import OPA
 from .rotator_factory import RotatorFactory
 from .eof_rotator import EOFRotator, ComplexEOFRotator
 from .mca_rotator import MCARotator, ComplexMCARotator
+from .cca import CCA
+
+
+__all__ = [
+    "EOF",
+    "ComplexEOF",
+    "MCA",
+    "ComplexMCA",
+    "OPA",
+    "RotatorFactory",
+    "EOFRotator",
+    "ComplexEOFRotator",
+    "MCARotator",
+    "ComplexMCARotator",
+    "CCA",
+]
