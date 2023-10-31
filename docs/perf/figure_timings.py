42d8e752de7d052a4f7e2b83eb17a534f1d3789c
Niclas Rieger <niclasrieger@gmail.com>
2023-11-01 00:21:57 +0100

    docs: add performance tests

---
+++ docs/perf/figure_timings.py
@@ -0,0 +1,134 @@
+# %%
+import xarray as xr
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import matplotx
+from matplotlib.colors import LogNorm
+
+# Load timings
+# =============================================================================
+timings = xr.open_dataset("timings.nc").sortby("n_features").sortby("n_samples")
+xeofs_timings = (
+    timings[["xeofs", "xeofs_dask"]].to_array("package").min(("run", "package"))
+)
+eofs_timings = (
+    timings[["eofs", "eofs_dask"]].to_array("package").min(("run", "package"))
+)
+ratio = xeofs_timings / eofs_timings
+# %%
+
+
+class MidPointLogNorm(LogNorm):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        LogNorm.__init__(self, vmin=vmin, vmax=vmax, clip=clip)
+        self.midpoint = midpoint
+
+    def __call__(self, value, clip=None):
+        result, is_scalar = self.process_value(value)
+        x, y = [np.log(self.vmin), np.log(self.midpoint), np.log(self.vmax)], [
+            0,
+            0.5,
+            1,
+        ]
+        return np.ma.array(np.interp(np.log(value), x, y), mask=result.mask, copy=False)
+
+
+def to_scientific_notation(arr):
+    return np.array([f"$10^{len(str(x))-1}$" if x != 0 else "0" for x in arr])
+
+
+# %%
+sns.set_context("paper")
+sns.set_style("ticks")
+
+
+def create_figure(path=None):
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+
+    ticks = [0.1, 1, 10, 60]
+    ticklabels = ["100 ms", "1 s", "10 s", "1 min"]
+    sns.heatmap(
+        timings["xeofs_dask"].min("run"),
+        ax=axes[0],
+        cmap="viridis",
+        norm=LogNorm(vmin=0.1, vmax=60),
+        cbar_kws=dict(ticks=ticks),
+    )
+    axes[0].collections[0].colorbar.ax.yaxis.set_ticks([], minor=True)
+    axes[0].collections[0].colorbar.set_ticklabels([t for t in ticklabels])
+    xcoords = timings["xeofs"]["n_features"].values
+    ycoords = timings["xeofs"]["n_samples"].values
+    xticklabels = to_scientific_notation(xcoords)
+    yticklabels = to_scientific_notation(ycoords)
+    xticks = np.arange(0.5, len(xcoords) + 0.5, 3)
+    yticks = np.arange(0.5, len(ycoords) + 0.5, 3)
+    axes[0].xaxis.set_ticks(xticks)
+    axes[0].yaxis.set_ticks(yticks)
+    axes[0].xaxis.set_ticklabels(xticklabels[::3])
+    axes[0].yaxis.set_ticklabels(yticklabels[::3], rotation=0)
+    axes[0].set_xlabel("Number of features")
+    axes[0].set_ylabel("Number of samples")
+    axes[0].set_title("A | Runtime xeofs", loc="left")
+
+    ticks = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    ticklabels = [
+        "x1000",
+        "x100",
+        "x10",
+        "same",
+        "x10",
+        "x100",
+        "x1000",
+    ]
+
+    sns.heatmap(
+        ratio,
+        ax=axes[1],
+        cmap="coolwarm",
+        norm=MidPointLogNorm(vmin=0.001, vmax=1000, midpoint=1),
+        cbar_kws=dict(ticks=ticks),
+    )
+    axes[1].collections[0].colorbar.ax.yaxis.set_ticks([], minor=True)
+    axes[1].collections[0].colorbar.set_ticklabels([t for t in ticklabels])
+    axes[1].collections[0].colorbar.set_label(
+        "Speed\nGain", rotation=0, labelpad=40, va="center", ha="right"
+    )
+    xcoords = timings["xeofs"]["n_features"].values
+    ycoords = timings["xeofs"]["n_samples"].values
+    xticklabels = to_scientific_notation(xcoords)
+    yticklabels = to_scientific_notation(ycoords)
+    xticks = np.arange(0.5, len(xcoords) + 0.5, 3)
+    yticks = np.arange(0.5, len(ycoords) + 0.5, 3)
+    axes[1].xaxis.set_ticks(xticks)
+    axes[1].yaxis.set_ticks(yticks)
+    axes[1].xaxis.set_ticklabels(xticklabels[::3])
+    axes[1].yaxis.set_ticklabels(yticklabels[::3], rotation=0)
+    axes[1].set_xlabel("Number of features")
+    axes[1].set_ylabel("Number of samples")
+    axes[1].text(1.35, 1, "eofs", transform=axes[1].transAxes, ha="left", va="center")
+    axes[1].text(1.35, 0, "xeofs", transform=axes[1].transAxes, ha="left", va="center")
+    x = timings.n_features.values
+    y = timings.n_samples.values
+    xx, yy = np.meshgrid(np.arange(0.5, len(x) + 0.5), np.arange(0.5, len(y) + 0.5))
+    axes[1].contour(
+        xx,
+        yy,
+        timings["nbytes"],
+        levels=[3e6],
+        linestyles="--",
+        colors="k",
+        linewidths=0.5,
+    )
+    axes[1].set_title("B | Speed gain eofs vs xeofs", loc="left")
+    plt.tight_layout()
+    if path is not None:
+        fig.savefig(path, dpi=200)
+    plt.show()
+
+
+# %%
+create_figure("timings_light.png")
+with plt.style.context(matplotx.styles.aura["dark"]):
+    create_figure("timings_dark.png")
+# %%
---
+++ docs/perf/xeofs_timings.py
@@ -0,0 +1,194 @@
+# %%
+import numpy as np
+import eofs
+import xeofs as xe
+import xarray as xr
+import dask
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import timeit
+
+
+# %%
+def fit_eofs(X, n_modes=2):
+    _ = eofs.xarray.Eof(X)
+
+
+def fit_xeofs(X, n_modes=2):
+    model = xe.models.EOF(n_modes=n_modes, random_state=5)
+    model.fit(X, dim="time")
+
+
+def create_test_data(ns, nf1, nf2, delayed=True, seed=None):
+    size = ns * nf1 * nf2
+    alpha = size / 1e7
+    n_chunks = int(alpha) + 1 if alpha > 1 else 1
+    chunk_size = int(np.ceil(ns / n_chunks))
+
+    rng = dask.array.random.default_rng(seed)
+    X = rng.standard_normal(
+        (ns, nf1, nf2), chunks=(chunk_size, -1, -1), dtype=np.float32
+    )
+    X = xr.DataArray(
+        X,
+        dims=("time", "lon", "lat"),
+        coords={
+            "time": np.arange(ns),
+            "lon": np.arange(nf1),
+            "lat": np.arange(nf2),
+        },
+        name="test_data",
+    )
+    if delayed:
+        return X
+    else:
+        return X.load()
+
+
+# %%
+
+MAX_MEMORY = {"eofs": 1000, "xeofs": 3000}
+n_repeats = 3
+
+n_samples = np.array([10, 20, 60, 100, 200, 600, 1_000, 2_000, 6_000, 10_000])[::-1]
+n_features_x = np.array(
+    [1, 2, 6, 10, 20, 60, 100, 200, 600, 1_000, 2_000, 6_000, 10_000]
+)[::-1]
+
+timings = {
+    "data": np.empty((len(n_samples), len(n_features_x))) * np.nan,
+    "eofs": np.empty((len(n_samples), len(n_features_x), n_repeats)) * np.nan,
+    "xeofs": np.empty((len(n_samples), len(n_features_x), n_repeats)) * np.nan,
+    "eofs_dask": np.empty((len(n_samples), len(n_features_x), n_repeats)) * np.nan,
+    "xeofs_dask": np.empty((len(n_samples), len(n_features_x), n_repeats)) * np.nan,
+}
+
+for i, ns in enumerate(n_samples):
+    print(f"n_samples = {ns}")
+    for j, nf in tqdm(enumerate(n_features_x), total=len(n_features_x)):
+        # Create test data
+        X = create_test_data(ns, nf, 10, delayed=True, seed=8)
+        nbytes = X.nbytes / 1e6  # in MiB
+
+        # Save data size
+        timings["data"][i][j] = X.nbytes
+
+        # Set number of loops
+        if 10 < nbytes:
+            n_loops = 1
+        elif 1 < nbytes and nbytes <= 10:
+            n_loops = 10
+        elif nbytes <= 1:
+            n_loops = 100
+        else:
+            n_loops = 1
+
+        # eofs / Dask
+        try:
+            if nbytes < 10_000:  # There seems to be a limit with eofs / Dask
+                print("eofs / Dask ...")
+                t_eofs = timeit.Timer(lambda: fit_eofs(X))
+                timings["eofs_dask"][i][j] = (
+                    np.array(t_eofs.repeat(n_repeats, n_loops)) / n_loops
+                )
+        except Exception as e:
+            print(f"Warning: eofs Dask failed for {ns} samples / {nf} features")
+            print("Error message: ", e)
+
+        # xeofs / Dask
+        try:
+            print("xeofs / Dask ...")
+            t_xeofs = timeit.Timer(lambda: fit_xeofs(X))
+            timings["xeofs_dask"][i][j] = (
+                np.array(t_xeofs.repeat(n_repeats, n_loops)) / n_loops
+            )
+        except Exception as e:
+            print(f"Warning: xeofs Dask failed for {ns} samples / {nf} features")
+            print("Error message: ", e)
+
+        # eofs / no Dask
+        if nbytes < MAX_MEMORY["eofs"]:
+            try:
+                print("eofs ...")
+                X = X.load()
+                t_eofs = timeit.Timer(lambda: fit_eofs(X))
+                timings["eofs"][i][j] = (
+                    np.array(t_eofs.repeat(n_repeats, n_loops)) / n_loops
+                )
+            except Exception as e:
+                print(f"Warning: eofs failed for {ns} samples / {nf} features")
+                print("Error message: ", e)
+
+        # xeofs / no Dask
+        if nbytes < MAX_MEMORY["xeofs"]:
+            try:
+                print("xeofs ...")
+                X = X.load()
+                t_xeofs = timeit.Timer(lambda: fit_xeofs(X))
+                timings["xeofs"][i][j] = (
+                    np.array(t_xeofs.repeat(n_repeats, n_loops)) / n_loops
+                )
+            except Exception as e:
+                print(f"Warning: xeofs failed for {ns} samples / {nf} features")
+                print("Error message: ", e)
+
+# %%
+# Save timings
+da_size = xr.DataArray(
+    timings["data"],
+    dims=("n_samples", "n_features"),
+    coords={"n_samples": n_samples, "n_features": n_features_x * 10},
+    name="nbytes",
+)
+
+
+da1 = xr.DataArray(
+    timings["eofs"],
+    dims=("n_samples", "n_features", "run"),
+    coords={
+        "n_samples": n_samples,
+        "n_features": n_features_x * 10,
+        "run": np.arange(n_repeats),
+    },
+    name="time",
+)
+da2 = xr.DataArray(
+    timings["xeofs"],
+    dims=("n_samples", "n_features", "run"),
+    coords={
+        "n_samples": n_samples,
+        "n_features": n_features_x * 10,
+        "run": np.arange(n_repeats),
+    },
+    name="time",
+)
+
+da3 = xr.DataArray(
+    timings["eofs_dask"],
+    dims=("n_samples", "n_features", "run"),
+    coords={
+        "n_samples": n_samples,
+        "n_features": n_features_x * 10,
+        "run": np.arange(n_repeats),
+    },
+    name="time",
+)
+da4 = xr.DataArray(
+    timings["xeofs_dask"],
+    dims=("n_samples", "n_features", "run"),
+    coords={
+        "n_samples": n_samples,
+        "n_features": n_features_x * 10,
+        "run": np.arange(n_repeats),
+    },
+    name="time",
+)
+
+# %%
+ds = xr.Dataset(
+    {"nbytes": da_size, "eofs": da1, "xeofs": da2, "eofs_dask": da3, "xeofs_dask": da4}
+)
+ds.to_netcdf("timings.nc")
+
+
+# %%
