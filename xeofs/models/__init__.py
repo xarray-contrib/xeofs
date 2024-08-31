0930844afb7992dd5ca8d00a615a69d096f84025
Niclas Rieger <niclasrieger@gmail.com>
2024-08-31 15:19:09 +0200

    docs: add deprecation warning

--- xeofs/models/__init__.py
+++ xeofs/models/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from .cca import CCA
 from .eeof import ExtendedEOF
 from .eof import EOF, HilbertEOF
@@ -25,3 +27,28 @@ __all__ = [
     "RotatorFactory",
     "SparsePCA",
 ]
+
+
+DEPRECATED_NAMES = [
+    ("ComplexEOF", "HilbertEOF"),
+    ("ComplexMCA", "HilbertMCA"),
+    ("ComplexEOFRotator", "HilbertEOFRotator"),
+    ("ComplexMCARotator", "HilbertMCARotator"),
+]
+
+
+def __dir__():
+    return sorted(__all__ + [names[0] for names in DEPRECATED_NAMES])
+
+
+def __getattr__(name):
+    for old_name, new_name in DEPRECATED_NAMES:
+        if name == old_name:
+            msg = (
+                f"Class '{old_name}' is deprecated and will be renamed to '{new_name}' in the next major release. "
+                f"In that release, '{old_name}' will refer to a different class. "
+                f"Please switch to '{new_name}' to maintain compatibility."
+            )
+            warnings.warn(msg, DeprecationWarning, stacklevel=2)
+            return globals()[new_name]
+    raise AttributeError(f"module {__name__} has no attribute {name}")
